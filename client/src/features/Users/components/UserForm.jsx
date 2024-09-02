55056361eeb9f1f9aedd5939d62c2d461b761190
deJames-13 <de.james013@gmail.com>
2024-09-02 17:12:32 +0800

    helo world

--- client/src/features/Users/components/UserForm.jsx
+++ client/src/features/Users/components/UserForm.jsx
@@ -3,7 +3,7 @@ import { PageTitle } from '@partials';
 function UserForm() {
   return (
     <>
-      <div className="border border-red-400 w-full h-full">
+      <div className=" w-full h-full">
         <PageTitle title="Users Form" />
       </div>
     </>
--- client/src/features/Users/components/UserList.jsx
+++ client/src/features/Users/components/UserList.jsx
@@ -3,7 +3,7 @@ import { PageTitle } from '@partials';
 function UserList() {
   return (
     <>
-      <div className="border border-red-400 w-full h-full">
+      <div className="w-full h-full">
         <PageTitle title="Users Lists" />
       </div>
     </>
