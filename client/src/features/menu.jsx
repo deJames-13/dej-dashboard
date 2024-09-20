e50a334c07911cda457cf7475dca35ffb7846a06
deJames-13 <ail>
2024-09-20 23:43:53 +0800

    s

--- client/src/features/menu.jsx
+++
@@ -1,9 +0,0 @@
-import { _exampleMenus } from './_Examples';
-import { userMenus } from './Users';
-
-export default [
-  // NEW ROUTE HERE ->
-  ...userMenus,
-  ..._exampleMenus,
-];
-
--- client/src/features/private.routes.js
+++
@@ -1,9 +0,0 @@
-import { _exampleRoutes } from './_Examples';
-import { userRoutes } from './Users';
-
-export default [
-  // NEW ROUTE HERE ->
-  ...userRoutes,
-  ..._exampleRoutes,
-];
-
